372b34e381e4ea6dec04fe82ac8a867d2870c290
Joel Onwuanaku <joelebuka@gmail.com>
2026-04-30 12:46:19 -0400

    style: update event card component to use real data

--- src/app/templates/components/event-card.js
+++ src/app/templates/components/event-card.js
@@ -1,28 +1,29 @@
 export function renderEventCard(event = {}) {
   const {
-    title = 'Event Title',
+    title = '',
     description = '',
-    day = '01',
-    month = 'Jan',
-    year = '2026',
-    location = 'Location',
-    time = 'Time',
+    day = '',
+    month = '',
+    year = '',
+    location = '',
+    time = '',
     href = '#',
   } = event;
+
+  const dateStr = [day, month, year].filter(Boolean).join(' ');
+  const metaItems = [dateStr, time, location].filter(Boolean);
+  const meta = metaItems.length
+    ? `<p class="event-details">${metaItems.join(' &nbsp; &#9679; &nbsp; ')}</p>`
+    : '';
 
   return `
     <a class="event-card" href="${href}">
       <article class="event-card__inner">
-        <div class="event-card__media">
-          <img class="event-card__media-img" src="/public/uploads/images/gblga-bhm-ig.png" alt="${title}" />
-        </div>
+        <div class="event-card__media"></div>
         <div class="event-card__body">
           <h3 class="event-card__title">${title}</h3>
           <p class="event-card__description">${description}</p>
-          <hr class="event-card__divider" />
-          <div class="event-card__meta">
-            <p class="event-details">${day} ${month}, ${year} &nbsp; &#9679; &nbsp; ${time} &nbsp; &#9679; &nbsp; ${location}</p>
-          </div>
+          ${meta ? `<hr class="event-card__divider" /><div class="event-card__meta">${meta}</div>` : ''}
           <span class="btn btn--primary event-card__cta">RSVP</span>
         </div>
       </article>
